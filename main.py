ff17aecf419e910ed87d14c560009dc20a1ddf9e
Valentins Fedotovs <vtrader@inbox.lv>
2020-10-22 16:04:20 +0100

    Created 2 new functions, detected bug no first line after first guess

--- main.py
+++ main.py
@@ -16,12 +16,40 @@ Once the you guess the correct number, the game is over.
 Try to quess number in less than 20 attempts.
 """
 
+def pritty_print(memory_list: list) -> None:
+    # print("debug: mylist len:", len(mylist))
+    myrange = int(len(memory_list) / 3)  # float
+    end = myrange * 3
+    #for lines in range(myrange):
+    for i in range(3, end, 3):
+        print((i / 3) , memory_list[i - 3], memory_list[i - 2], memory_list[i - 1])
+
+
+def memorize_guesses(cug: str,bc :int,cc :int, mylist: list) ->list:
+    """Function converts variables to strings and appends to list
+
+    Parameters:
+    cug -> curr_usr_guess str
+    bc ->  bull_count int
+    cc ->  cow_count int
+    mylist -> previous guess history as list
+
+    Returns: mylist as list
+    """
+    bc_str = "bulls: " + str(bc)
+    cc_str = "cows: " + str(cc)
+    mylist.append(cug)
+    mylist.append(cc_str)
+    mylist.append(bc_str)
+    return mylist
+
 
 def main():
     print(game_rules)
     number_to_guess = gen_random_ints()
     attempt_count = 1
     max_attempt_count = 20
+    pritty_list = []
     while attempt_count < max_attempt_count:
         curr_usr_guess = get_user_guess()
         if check_win(number_to_guess, curr_usr_guess) is True:
@@ -31,8 +59,12 @@ def main():
         else:
             cow_count = check_cows(number_to_guess, curr_usr_guess)
             bull_count = check_bulls(number_to_guess, curr_usr_guess)
-            print("You have ", cow_count, " cows, ", bull_count,
-                  " bulls ", attempt_count, " attempts tryed")
+            #print("You have ", cow_count, " cows, ", bull_count,
+            #      " bulls ", attempt_count, " attempts tryed")
+            #print("---test ---")
+            guess_history = memorize_guesses(curr_usr_guess, cow_count, bull_count, pritty_list)
+            pritty_print(guess_history)
+
         attempt_count += 1
 
     if attempt_count == max_attempt_count:
